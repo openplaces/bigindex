664ac7dced378653dc134e75035b001c0190e978
Greg Lu <greg.lu@gmail.com>
2009-11-26 19:01:38 -0500

    Documentation update.
    * IndexField class documentation written.
    * Some minor updates to README

--- lib/big_index/index_field.rb
+++ lib/big_index/index_field.rb
@@ -10,9 +10,49 @@ module BigIndex
   # Although these IndexField objects aren't created manually, they're
   # implicitly created from calls to the {BigIndex::Model#index} macro.
   #
-  # At a minimum, an IndexField requires a name and will default to type "text".
-  # In this case, the name needs to refer to a valid method (of appropriate
-  # type), and will just index the return of the method.
+  # At a minimum, an IndexField requires a name, and will then default to type
+  # "text". In this case, the name needs to refer to a valid method (of
+  # appropriate type), and will just implicitly index the return of that method.
+  #
+  #  class Book < BigRecord::Base
+  #    include BigIndex::Resource
+  #
+  #    column :title, :string
+  #
+  #    index :title
+  #  end
+  #
+  # Illustrated in the example above, we have the attribute "title" for this
+  # given model Book, and we define an index field with just a the name :title.
+  # From this, BigIndex will reference the #title method and index the return
+  # of that method (string in this case) as the default index type :text.
+  #
+  # If a method doesn't exist for a given field name, then a block must be
+  # attached to the {BigIndex::Model#index} macro, like the following example:
+  #
+  #  class Book < BigRecord::Base
+  #    include BigIndex::Resource
+  #
+  #    column :title, :string
+  #
+  #    index :title
+  #    index :title_exact_match, :string do |book|
+  #      book.title
+  #    end
+  #  end
+  #
+  # The example above illustrates the same Book model as before, but now with a
+  # new index field defined named "title_exact_match". Field names, after all,
+  # can be named arbitrarily so a method might not exist for that name. The
+  # notable difference here is that there is a block defined that accepts the
+  # argument |book|. What happens in this case is that whenever indexing occurs
+  # for a given Book record, it will pass the record object to this block and
+  # index the returned value of the block. In this case, a new index field
+  # needed to be defined that would match exact title names (:string type fields
+  # cause exact matches), rather than the partial matches that get found from
+  # type :text. So although there's only one attribute for this model, there are
+  # many different ways we can index that value, and these decisions depend on
+  # your application and indexing system.
   #
   class IndexField
     include Assertions
@@ -48,6 +88,6 @@ module BigIndex
       @options[name.to_sym] || super
     end
 
-  end # class IndexField
+  end
 
 end # module BigIndex
